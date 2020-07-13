7a4d2c9d471b3be4d83f6162d4d897d05871d5f6
purnama <skindrapurnama@gmail.com>
2020-07-13 15:57:06 +0700

    ktegori

--- application/views/kategori/index.php
+++ application/views/kategori/index.php
@@ -29,46 +29,46 @@
         </div>
     </div>
 </form>
-    <?=form_open('kategori/ubahsubkategori',['class'=>"form-inline" ]);?>
-        <div class="form-group mb-2">
-            <label for="" class="control-label">Pilih Sub Kategori</label>
+<?=form_open('kategori/ubahsubkategori',['class'=>"form-inline" ]);?>
+    <div class="form-group mb-2">
+        <label for="" class="control-label">Pilih Sub Kategori</label>
+    </div>
+    <div class="form-group mx-sm-3 mb-2">
+        <?=form_dropdown('id_subkategori',getDropdownList('subkategori',['id_subkategori','nama_subkategori']),'',['class'=>'form-control','id'=>'subkategori']);?>
+    </div>
+    <div class="form-group mx-sm-2 mb-2">
+        <input type="text" class="form-control" id="ubahsubkategoribaru" name="nama_subkategori" placeholder="Ketik Ubah Sub Kategori">
+        <?=form_error('nama_subkategori');?>
+    </div>
+    <div class="btn-group mb-2">
+        <button type="button" class="btn btn-outline-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Pilih
+        </button>
+        <div class="dropdown-menu dropdown-menu-right">
+            <button class="dropdown-item text-info" type="submit">Ubah</button>
+            <a href="#"  class="hapus-subkategori dropdown-item text-danger">Hapus</a>
         </div>
-        <div class="form-group mx-sm-3 mb-2">
-            <?=form_dropdown('id_subkategori',getDropdownList('subkategori',['id_subkategori','nama_subkategori']),'',['class'=>'form-control','id'=>'subkategori']);?>
-        </div>
-        <div class="form-group mx-sm-2 mb-2">
-            <input type="text" class="form-control" id="ubahsubkategoribaru" name="nama_subkategori" placeholder="Ketik Ubah Sub Kategori">
-            <?=form_error('nama_subkategori');?>
-        </div>
-        <div class="btn-group mb-2">
-            <button type="button" class="btn btn-outline-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Pilih
-            </button>
-            <div class="dropdown-menu dropdown-menu-right">
-                <button class="dropdown-item text-info" type="submit">Ubah</button>
-                <a href="#"  class="hapus-subkategori dropdown-item text-danger">Hapus</a>
+    </div>
+</form>
+
+
+
+    <div class="modal  fade" id="ubahmodalkategori" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
             </div>
         </div>
-    </form>
-
-
-
-<div class="modal  fade" id="ubahmodalkategori" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
     </div>
-  </div>
-</div>
 
 
